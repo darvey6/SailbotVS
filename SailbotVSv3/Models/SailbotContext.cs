bd779a124da5b5866f09a4f298efd1c6951f320b
Darvey <darvey6@hotmail.com>
2019-08-28 21:59:53 -0700

    added gitignore

---
+++ SailbotVSv3/Models/SailbotContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace SailbotVSv3.Models
+{
+    public class SailbotContext : DbContext
+    {
+        public SailbotContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Wind> Wind { get; set; }
+    }
+}
---
+++ SailbotVSv3/Models/Wind.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SailbotVSv3.Models
+{
+    public class Wind
+    {
+        [Key]
+        public int SensorID { get; set; }
+        public string WindDirection { get; set; }
+        public bool WindReference { get; set; }
+        public int WindTemperature { get; set; }
+        public int UCCMCurrent { get; set; }
+        public int Voltage { get; set; }
+        public DateTime UpdatedTime { get; set; }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
